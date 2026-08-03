1b76b5325dd1b689e39014d792b3ef45cdee7112
vizek920 <ffzx975@gmail.com>
2026-08-03 16:16:16 +0300

    Delete frontend directory

--- frontend/src/socket.js
+++
@@ -1,9 +0,0 @@
-import { io } from "socket.io-client";
-
-// غيّر هذا الرابط لرابط السيرفر بتاعك على Render بعد النشر
-const SERVER_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:3001";
-
-export const socket = io(SERVER_URL, {
-  autoConnect: true,
-  transports: ["websocket", "polling"],
-});
